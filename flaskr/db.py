c021e62913c4ea7a12eb8f8444c683954c5fa2d9
LoganMercadillo <logan.mercadillo@gmail.com>
2022-07-25 14:39:50 -0500

    updated db.py comments

--- flaskr/db.py
+++ flaskr/db.py
@@ -3,25 +3,43 @@ import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+# NOTE: Nearly all comments are stolen/modified from
+# https://flask.palletsprojects.com/en/2.1.x/tutorial/database/.
+
+
+# First thing to do when working with databases: create a connection to it.
+# Any queries or operations performed on the database will be performed
+# using this connection, and the connection will close after the work is done.
+
+# The connection is created at some point when handling a request,
+# and it is closed before the response to the request is sent.
+
+
+# ############### #
+# GET_DB() NOTES: #
+# ############### #
 # 'g' is a special object that is unique for each request.
 # It is used to store data that might be accessed by multiple functions
 # during the request.
 # The connection is stored and reused instead of creating a new connection
 # if 'get_db()' is called a second time in the same request.
 
-# 'current_app' is another special object, it points to the Flask application
-# that is handling the request.
-# Because 'get_db()' will be called when the application has been created
-# and is handling a request, 'current_app' can be used.
-
 # 'sqlite3.connect()' establishes a connection to the file pointed at by
 # the DATABASE configuration key.
 
-# sqlite3.Row tells the connection to return the rows that behave like dicts.
-# this allows accessing of columns by name.
+# 'current_app' is another special object, it points to the Flask application
+# that is handling the request.
+# (Because 'get_db()' will be called when the application has been created
+# and is handling a request, 'current_app' can be used.)
 
-
+# 'sqlite3.Row' tells the connection to return the rows that behave like dicts.
+# This allows accessing of columns by name.
 def get_db():
+    """
+    Called when the application has been created and is handling a request.
+
+    Returns: a Connection object to the database
+    """
     if 'db' not in g:
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
