1b65a84343da837d7649ac34140fa9d08322ab44
LoganMercadillo <logan.mercadillo@gmail.com>
2022-07-28 09:40:11 -0500

    added register view to auth bp

--- auth.py
+++ auth.py
@@ -28,6 +28,7 @@
 # TLDR: THIS IS THE AUTHENTICATION BLUEPRINT.
 
 import functools
+from sqlite3 import IntegrityError
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -39,3 +40,38 @@ from flaskr.db import get_db
 # Create a blueprint named 'auth', defined in __name__ (auth.py),
 # and prepends '/auth' to all the URLs associated with this blueprint.
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+# REGISTER VIEW
+# associate URL "/register" with the register view function (register())
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+
+        if error is None:
+            try:
+                db.execute(
+                    f'INSERT INTO user (username, password) VALUES (?, ?)',
+                    (username, generate_password_hash(password))
+                )
+                db.commit()
+            # db.IntegrityError occurs when username already exists
+            except db.IntegrityError:
+                error = f'User {username} is already registered.'
+            else:
+                return redirect(url_for('auth.login'))
+
+        # if validation fails, show user
+        flash(error)
+
+    # initial display ('GET' request) or validation error
+    return render_template('auth/register.html')
