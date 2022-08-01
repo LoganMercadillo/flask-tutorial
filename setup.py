83bec01384df4bdb569ffc2c5ae3a193b7eb2859
LoganMercadillo <logan.mercadillo@gmail.com>
2022-08-01 17:15:02 -0500

    added delete view, setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from jinja2 import TemplateRuntimeError
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    
+)
